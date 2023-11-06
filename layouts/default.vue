f8bd5b9ef70508f429c0a16288d90a90ced6acd3
Apfirebolt <aspper20@gmail.com>
2023-11-07 04:35:44 +0530

    new page added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
     <v-app>
+        <p>
+            John Doe
+        </p>
         <core-drawer />
         <core-app-bar />
         <core-view>
---
+++ pages/About.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container fluid fill-height>
+        <v-row align="center" justify="center">
+            <v-col cols="12">
+                <material-card color="success">
+                    <template #header>
+                        <div class="px-3">
+                            <div class="title font-weight-light mb-2">About Page</div>
+
+                            <div class="category">
+                                Handcrafted by us with
+                                <v-icon size="17">mdi-heart</v-icon>
+                            </div>
+                        </div>
+                    </template>
+                </material-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        color: null,
+        colors: ['purple', 'info', 'success', 'warning', 'error'],
+        top: true,
+        bottom: false,
+        left: false,
+        right: false,
+        snackbar: false,
+    }),
+    
+};
+</script>
