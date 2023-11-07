951eaae5ba9dcc770b4162879f00af5bcd4a25a0
Apfirebolt <aspper20@gmail.com>
2023-11-07 08:33:35 +0530

    initial page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>We are a team of developers passionate about creating amazing web applications.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'About Us'
+  }
+}
+</script>
