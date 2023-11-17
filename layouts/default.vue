7c939e601adfac8e3e93287e1f16d255a955374f
Apfirebolt <aspper20@gmail.com>
2023-11-17 19:14:23 +0530

    feat: added experimental layouts

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div>
+    <header>
+      <p>
+        Default Layout
+      </p>
+    </header>
+
+    <footer>
+      <!-- Add your footer content here -->
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style>
+/* Add your custom styles for the layout here */
+</style>
---
+++ layouts/product.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <div>
+        <header>
+            <p>
+                Product Layout Page
+            </p>
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            <p>
+                some footer content
+            </p>
+        </footer>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'ProductLayout',
+}
+</script>
+  
+<style>
+/* Add your custom styles for the layout here */
+</style>
+  
